f1b1fb76a3941e3a6a6a5b975508bdf841d63628
joanna <jo.senczuk@gmail.com>
2018-03-07 12:53:47 +0000

    Change style, add avatart to doctor on doctor list

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -4,11 +4,14 @@ const Doctor = ({doctor}) => {
 
     return (
         <div className="doctor">
-            <div>
+            <img className="doctorListAvatar"
+                 src={"https://cdn3.iconfinder.com/data/icons/internet-and-web-4/78/internt_web_technology-13-256.png"}/>
+            <div className="doctorListInfo">
             <div className="doctorName">{doctor.firstName}</div>
             <div className="doctorName">{doctor.lastName}</div>
-            </div>
             <div>{doctor.specialty}</div>
+            </div>
+            <button class="btn btn-outline-primary" type="submit">Button</button>
         </div>
     )
 };
