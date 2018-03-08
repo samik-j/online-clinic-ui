fdf12bc97f1d4ac53e33fd6ea10b032b700acd37
joanna <jo.senczuk@gmail.com>
2018-03-08 19:23:35 +0000

    Add appointments to DoctorPage

--- src/components/DoctorPage.js
+++ src/components/DoctorPage.js
@@ -10,18 +10,27 @@ class DoctorPage extends React.Component {
         super(props);
 
         this.state = {
-            doctor: ''
+            doctor: '',
+            appointments: []
         }
     }
 
     componentDidMount() {
-        axios.get(`${DOCTORS_URL}/${this.props.match.params.doctorId}`).then(
-            response => {
-                this.setState({
-                    doctor: response.data
-                })
-            }
-        );
+        axios.get(`${DOCTORS_URL}/${this.props.match.params.doctorId}`)
+            .then(
+                response => {
+                    this.setState({
+                        doctor: response.data
+                    })
+                }
+            );
+        axios.get(`${DOCTORS_URL}/${this.props.match.params.doctorId}/appointments`)
+            .then(
+                response => {
+                    this.setState({
+                        appointments: response.data
+                    })
+                });
     }
 
     render() {
@@ -31,13 +40,35 @@ class DoctorPage extends React.Component {
                     <img className="doctor-avatar"
                          src={"/img/doctor_avatar.jpg"}
                          alt="Doctor"/>
-                        <div className="doctor-info">
-                            <div className="doctor-name">{this.state.doctor.firstName}</div>
-                            <div className="doctor-name">{this.state.doctor.lastName}</div>
-                            <div className="doctor-specialty">{this.state.doctor.specialty}</div>
-                            <Link className="btn btn-primary book-btn" to={`/doctors/${this.state.doctor.id}/appointments`}>Book
-                                appointment</Link>
-                        </div>
+                    <div className="doctor-info">
+                        <div className="doctor-name">{this.state.doctor.firstName}</div>
+                        <div className="doctor-name">{this.state.doctor.lastName}</div>
+                        <div className="doctor-specialty">{this.state.doctor.specialty}</div>
+                        <Link className="btn btn-primary book-btn" to={`/doctors/${this.state.doctor.id}/appointments`}>Book
+                            appointment</Link>
+                    </div>
+                    <div className="container">
+                        <table className="table">
+                            <thead>
+                            <tr>
+                                <th>dATE</th>
+                                <th>TIME</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                this.state.appointments.map(appointment => {
+                                    return (
+                                        <tr key={appointment.id}>
+                                            <td>{appointment.date}</td>
+                                            <td>{appointment.time}</td>
+                                        </tr>)
+                                })
+                            }
+                            </tbody>
+                        </table>
+
+                    </div>
                 </div>
             </div>
         )
