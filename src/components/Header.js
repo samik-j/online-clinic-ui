0673d61e1ad034432a85a7f8975558cfecf6d7f1
joanna <jo.senczuk@gmail.com>
2018-03-08 17:13:47 +0000

    Add link to logo to Home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
 
     return (
         <header>
             <div className="page-width">
+                <Link to="/">
             <img className="logo inline"
                  src={"https://preview.ibb.co/nAHNf7/caduceus.png"}
                  alt="Caduceus"/>
-            <div className="inline">Online Clinic</div>
+                </Link>
+                <div className="inline">Online Clinic</div>
             </div>
         </header>
     )
