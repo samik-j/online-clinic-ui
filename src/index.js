fc6ce5ce3aeaddd97b507abb5f4136da5dd24c06
joanna <jo.senczuk@gmail.com>
2018-03-08 16:57:41 +0000

    Add Home component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 
 //COMPONENTS
 import Header from './components/Header';
+import Home from './components/Home';
 import DoctorListPage from './components/DoctorListPage';
 import DoctorPage from './components/DoctorPage';
 import AppointmentsPage from './components/AppointmentsPage';
@@ -31,6 +32,7 @@ const App = () => {
                             <Route path="/doctors/:doctorId/appointments" component={AppointmentsPage}/>
                             <Route path="/doctors/:doctorId" component={DoctorPage}/>
                             <Route path="/doctors" component={DoctorListPage}/>
+                            <Route path="/" component={Home}/>
                             <Route path="*" component={NotFound}/>
                         </Switch>
                     </article>
