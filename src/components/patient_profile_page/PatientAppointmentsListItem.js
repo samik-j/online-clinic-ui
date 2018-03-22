117ca3fee22356c464ee9b70c1c7ad916cde2968
joanna <jo.senczuk@gmail.com>
2018-03-22 16:14:35 +0000

    Remove unused code

--- src/components/patient_profile_page/PatientAppointmentsListItem.js
+++ src/components/patient_profile_page/PatientAppointmentsListItem.js
@@ -4,45 +4,35 @@ import { Link } from 'react-router-dom'
 
 const PatientAppointmentsListItem = ({appointment}) => {
 
-    const item =
-        <div>
-            <td className="date">{moment(appointment.date).format('ddd DD.MM.YYYY')}</td>
-            <td className="time">{appointment.time}</td>
-            <td className="doctor"><Link to={`/doctors/${appointment.doctorId}`}>{appointment.doctorId}</Link></td>
-            <td className="reason">{appointment.reason}</td>
-        </div>
-
     var imgSrc = ''
-    var style = ''
     var status = ''
+
     if (appointment.status === 'CANCELLED') {
         imgSrc = '/img/status_cancelled.png'
-        style = "status cancelled"
         status = 'Cancelled'
-    } else if (appointment.status === 'NOT_CONFIRMED'){
+    } else if (appointment.status === 'NOT_CONFIRMED') {
         imgSrc = '/img/status_not_confirmed.png'
-        style = "status not-confirmed"
         status = 'Not Confirmed'
-    } else if (appointment.status === 'CONFIRMED'){
+    } else if (appointment.status === 'CONFIRMED') {
         imgSrc = '/img/status_confirmed.png'
-        style = "status confirmed"
         status = 'Confirmed'
     }
 
     return (
         <div className="appointment">
-        <table id="appointment">
-            <tbody>
-            <tr>
-                <td className="date">{moment(appointment.date).format('ddd DD.MM.YYYY')}</td>
-                <td className="time">{appointment.time}</td>
-                <td className="doctor"><Link to={`/doctors/${appointment.doctorId}`}>{appointment.doctorName}</Link></td>
-                <td className="reason">{appointment.reason}</td>
-                <td className={style}>{status}</td>
-                <td className="status-img"><img className="status-img" src={imgSrc} alt="status"/></td>
-            </tr>
-            </tbody>
-        </table>
+            <table id="appointment">
+                <tbody>
+                <tr>
+                    <td className="date">{moment(appointment.date).format('ddd DD.MM.YYYY')}</td>
+                    <td className="time">{appointment.time}</td>
+                    <td className="doctor"><Link to={`/doctors/${appointment.doctorId}`}>{appointment.doctorName}</Link>
+                    </td>
+                    <td className="reason">{appointment.reason}</td>
+                    <td className="status">{status}</td>
+                    <td className="status-img"><img className="status-img" src={imgSrc} alt="status"/></td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
