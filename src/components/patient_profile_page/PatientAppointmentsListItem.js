e962c9143d1d469a02bb7e2ce6202d59727082b8
joanna <jo.senczuk@gmail.com>
2018-03-23 15:08:57 +0000

    Change status images to svg elements

--- src/components/patient_profile_page/PatientAppointmentsListItem.js
+++ src/components/patient_profile_page/PatientAppointmentsListItem.js
@@ -6,16 +6,34 @@ const PatientAppointmentsListItem = ({appointment}) => {
 
     var imgSrc = ''
     var status = ''
+    var statusSymbol = ''
 
     if (appointment.status === 'CANCELLED') {
-        imgSrc = '/img/status_cancelled.png'
         status = 'Cancelled'
+        statusSymbol =
+            <svg fill="#c82424" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 0h24v24H0z" fill="none"/>
+                <path
+                    d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
+            </svg>
+
     } else if (appointment.status === 'NOT_CONFIRMED') {
-        imgSrc = '/img/status_not_confirmed.png'
         status = 'Not Confirmed'
+        statusSymbol =
+            <svg fill="#ffde00" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>
+                <path d="M0 0h24v24H0z" fill="none"/>
+            </svg>
+
     } else if (appointment.status === 'CONFIRMED') {
-        imgSrc = '/img/status_confirmed.png'
         status = 'Confirmed'
+        statusSymbol =
+            <svg fill="#0e9700" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 0h24v24H0z" fill="none"/>
+                <path
+                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+            </svg>
     }
 
     return (
@@ -29,7 +47,7 @@ const PatientAppointmentsListItem = ({appointment}) => {
                     </td>
                     <td className="reason">{appointment.reason}</td>
                     <td className="status">{status}</td>
-                    <td className="status-img"><img className="status-img" src={imgSrc} alt="status"/></td>
+                    <td className="status-symbol">{statusSymbol}</td>
                 </tr>
                 </tbody>
             </table>
