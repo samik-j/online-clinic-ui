05243faf2ed337b191257720bba5c172eb502c08
joanna <jo.senczuk@gmail.com>
2018-04-05 16:49:27 +0100

    Add content to AppointmentInformation

--- src/components/book_appointment_page/AppointmentInformation.js
+++ src/components/book_appointment_page/AppointmentInformation.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
 
 const AppointmentInformation = ({appointment}) => {
 
     return (
-        <div>
-            {appointment.id}
-            Appointment Information
+        <div className="appointment-info">
+            <img className="icon"
+                 src={'/img/calendar.svg'}
+                 alt="callendar"/>
+            <div className="info">
+                <div className="weekday">{moment(appointment.date).format('dddd')}</div>
+                <div>{moment(appointment.date).format('DD MMMM YYYY')}</div>
+                <div>{appointment.time}</div>
+                <Link to={`/doctors/${appointment.doctorId}`} className="link">Choose other appointment</Link>
+            </div>
         </div>
-    )
+    );
 
 };
 
