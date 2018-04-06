0d716415f907be0bbaef3a14e96bb41822fe05f8
joanna <jo.senczuk@gmail.com>
2018-04-06 12:39:21 +0100

    Add dropdown to AddAppointmentpage

--- src/components/add_appointment_page/AddAppointmentPage.js
+++ src/components/add_appointment_page/AddAppointmentPage.js
@@ -43,6 +43,64 @@ class AddAppointmentPage extends React.Component {
         event.preventDefault();
     }
 
+    hourDropDown = () => {
+        return (
+            <div className="time-input">
+                <select id="specialty" name="specialty" className="form-control"
+                        value={this.state.hour} onChange={this.handleHourChange}>
+                    <option hidden value=""> </option>
+                    <option value="00">0</option>
+                    <option value="01">1</option>
+                    <option value="02">2</option>
+                    <option value="03">3</option>
+                    <option value="04">4</option>
+                    <option value="05">5</option>
+                    <option value="06">6</option>
+                    <option value="07">7</option>
+                    <option value="08">8</option>
+                    <option value="09">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                    <option value="21">21</option>
+                    <option value="22">22</option>
+                    <option value="23">23</option>
+                </select>
+            </div>
+        );
+    };
+
+    minutesDropDown = () => {
+        return (
+            <div className="time-input">
+                <select id="specialty" name="specialty" className="form-control"
+                        value={this.state.minutes} onChange={this.handleMinutesChange}>
+                    <option hidden value=""> </option>
+                    <option value="00">00</option>
+                    <option value="05">05</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                    <option value="25">25</option>
+                    <option value="30">30</option>
+                    <option value="35">35</option>
+                    <option value="40">40</option>
+                    <option value="45">45</option>
+                    <option value="50">50</option>
+                    <option value="55">55</option>
+                </select>
+            </div>
+        );
+    };
+
     render () {
         return (
             <div className="page-width">
@@ -57,17 +115,10 @@ class AddAppointmentPage extends React.Component {
                                 <div className="weekday">{moment(this.state.date).format('dddd')}</div>
                                 <div>{moment(this.state.date).format('DD MMMM YYYY')}</div>
                                 <form onSubmit={this.handleSubmit}>
-                                    <div>
-                                        <div className="time-input">
-                                            <input className="input" type="text" name="hour" value={this.state.hour}
-                                                   onChange={this.handleHourChange}/>
-                                        </div>
+                                    <div className="time">
+                                        {this.hourDropDown()}
                                         :
-                                        <div className="time-input">
-                                            <input className="input" type="text" name="minutes"
-                                                   value={this.state.minutes}
-                                                   onChange={this.handleMinutesChange}/>
-                                        </div>
+                                        {this.minutesDropDown()}
                                     </div>
                                     <input className="btn btn-primary" type="submit" value="Add"/>
                                 </form>
