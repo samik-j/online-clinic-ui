4d80c64e4087702bf498bdd75ceba7a8d439754a
joanna.senczuk <joanna.senczuk@ocado.com>
2018-11-23 14:58:26 +0000

    Change urls

--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,3 @@
-export const DOCTORS_URL = 'http://0.0.0.0:8087/doctors';
-export const PATIENTS_URL = 'http://0.0.0.0:8087/patients';
-export const APPOINTMENTS_BOOKED_URL = 'http://0.0.0.0:8087/appointmentsBooked';
+export const DOCTORS_URL = 'http://localhost:8080/doctors';
+export const PATIENTS_URL = 'http://localhost:8080/patients';
+export const APPOINTMENTS_BOOKED_URL = 'http://localhost:8080/appointmentsBooked';
